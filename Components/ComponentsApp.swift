67735a8820742e02bd09e84914cfb77f3a2887a6
Avinash Tag <avi.tag@outlook.com>
2024-10-03 12:17:09 +0800

    Initial Commit

---
+++ Components/ComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentsApp.swift
+//  Components
+//
+//  Created by Avinash on 03/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
