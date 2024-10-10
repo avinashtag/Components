c82bc103320e4e66e711cadce4f533df30f7c8e7
kp <karthi.neel81@gmail.com>
2024-10-09 19:35:25 -0700

    Added Tint color blue for tab bar

---
+++ Components/Cart View/CartView.swift
@@ -0,0 +1,18 @@
+//
+//  CartView.swift
+//  Components
+//
+//  Created by kp on 10/9/24.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartView()
+}
