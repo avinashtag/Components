6915f82aed646122efb3fbd87579d656d575cd6d
Avinash Tag <avi.tag@outlook.com>
2024-11-05 23:44:44 +0800

    map

--- Components/List View/BeantownButton.swift
+++ Components/List View/BeantownButton.swift
@@ -6,13 +6,71 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct BeantownButton: View {
+    
+    @Binding var searchResults: [MKMapItem]
+    @Binding var position: MapCameraPosition
+    @Binding var visibleRegion: MKCoordinateRegion?
+
+    func search(for query: String) {
+        let request = MKLocalSearch.Request ()
+        request.naturalLanguageQuery = query
+        request.resultTypes = .pointOfInterest
+        request.region = visibleRegion ?? MKCoordinateRegion(center: .parking, span: MKCoordinateSpan(latitudeDelta: 0.0125, longitudeDelta: 0.0125))
+        
+        Task{
+            let search = MKLocalSearch(request: request)
+            let response = try? await search.start()
+            searchResults = response?.mapItems ?? []
+        }
+
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        HStack{
+            Button(action: {
+                search(for: "Playgrounds")
+            }, label: {
+                Label("Playgrounds", systemImage: "figure.and.child.holdinghands")
+            })
+            .buttonStyle(.borderedProminent)
+            Button(action: {
+                search(for: "Beach")
+            }, label: {
+                Label("Beaches", systemImage: "beach.umbrella")
+            })
+            .buttonStyle(.borderedProminent)
+            
+            Button(action: {
+                position = .region(.boston)
+            }, label: {
+                Label("Boston", systemImage: "bulding.2")
+            })
+            .buttonStyle(.borderedProminent)
+
+            Button(action: {
+                position = .region(.northShore)
+            }, label: {
+                Label("North Shore", systemImage: "water.waves")
+            })
+            .buttonStyle(.borderedProminent)
+
+        }
+        .labelStyle(.iconOnly)
     }
 }
 
 #Preview {
-    BeantownButton()
+    BeantownButton(searchResults: Binding(get: {
+        []
+    }, set: { _ in
+        
+    }), position: Binding(get: {.automatic}, set: { _ in
+        
+    }), visibleRegion: Binding(get: { nil }, set: { _ in
+        
+    }))
 }
--- Components/List View/ProductMapView.swift
+++ Components/List View/ProductMapView.swift
@@ -17,9 +17,35 @@ struct Location : Identifiable{
     var coordinate: CLLocationCoordinate2D
 }
 
+extension CLLocationCoordinate2D{
+    static let parking = CLLocationCoordinate2D(latitude: 42.354528, longitude: -71.068369)
+}
+
+extension MKCoordinateRegion {
+    
+    static let boston = MKCoordinateRegion ( center: CLLocationCoordinate2D( latitude: 42.360256,
+                                                                             longitude: -71.057279), span: MKCoordinateSpan ( latitudeDelta: 0.1,
+                                                                                                                              longitudeDelta: 0.1))
+    
+    static let northShore = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 42.547408,
+            longitude: -70.870085),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.5,
+            longitudeDelta: 0.5))
+    
+}
+
 struct ProductMapView: View {
     
     @State private  var cameraPosition : MapCameraPosition = .automatic
+    @State var searchResults: [MKMapItem] = []
+    @State var visibleRegion: MKCoordinateRegion?
+    @State var selected: MKMapItem?
+    @State var route: MKRoute?
+
+    
     
     @State var locations = [
         Location(name: "Kuala Lumpur", coordinate: CLLocationCoordinate2DMake(3.1319, 101.6841)),
@@ -28,34 +54,79 @@ struct ProductMapView: View {
     var body: some View {
         
         MapReader { proxy in
-            Map(position: $cameraPosition){
-                ForEach(locations) { location in
-    //                Marker(location.name, coordinate: location.coordinate)
-                    
-                    Annotation(location.name, coordinate: location.coordinate) {
-                        HStack{
-                            Image(systemName: "car")
-                                .resizable()
-                                .frame(width: 30)
-                                .foregroundStyle(.red)
-                            Text(location.name)
-                                .font(.headline)
-                                .foregroundStyle(.red)
-
-                        }
+            
+            Map(position: $cameraPosition, selection: $selected){
+                Annotation("Parking", coordinate: .parking) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(.background)
+                        RoundedRectangle (cornerRadius: 5)
+                            .stroke(.secondary, lineWidth: 5)
+                        Image (systemName: "car")
+                            .padding (5)
                     }
-                    
                 }
-            }
-            .onTapGesture { position in
-                if let coordinate = proxy.convert(position, from: .local){
-                    locations.append(Location(name: "Custom", coordinate: coordinate))
+//                .annotationTitles(.hidden)
+                
+                ForEach(searchResults, id: \.self) { result in
+                    Marker(item: result)
+                }
+                .annotationTitles(.hidden)
+                
+                if let route{
+                    MapPolyline(route)
+                        .stroke(.blue, lineWidth: 5)
                 }
             }
         }
-
-        .mapStyle(.standard)
+        .mapControls({
+            MapUserLocationButton()
+            MapCompass()
+            MapScaleView()
+        })
+        .mapStyle(.standard(elevation: .realistic))
+        .safeAreaInset(edge: .bottom) {
+            HStack{
+                Spacer()
+                VStack(spacing: 0, content: {
+                    if let selected{
+                        MapItemDetails(route: $route, selected: $selected)
+                            .frame(height: 150)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .padding([.top, .horizontal])
+                    }
+                    BeantownButton(searchResults: $searchResults, position: $cameraPosition, visibleRegion: $visibleRegion)
+                        .padding(.top)
+                })
+                
+                Spacer()
+            }
+            .background(.thinMaterial)
+        }
+        .onChange(of: searchResults) {
+            cameraPosition = .automatic
+        }
+        .onMapCameraChange { context in
+            visibleRegion = context.region
+        }
         
+    }
+    
+    
+    
+    func getDirections(){
+        route = nil
+        guard let selected else { return }
+        
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: .parking))
+        request.destination = selected
+        
+        Task{
+            let directions = MKDirections(request: request)
+            let response = try? await directions.calculate()
+            route = response?.routes.first
+        }
     }
 }
 
