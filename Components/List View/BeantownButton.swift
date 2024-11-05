e6a794a5c534c9505d9cd49828f4456298b1a970
Avinash Tag <avi.tag@outlook.com>
2024-11-05 23:44:32 +0800

    map

---
+++ Components/List View/BeantownButton.swift
@@ -0,0 +1,18 @@
+//
+//  BeantownButton.swift
+//  Components
+//
+//  Created by Avinash on 05/11/2024.
+//
+
+import SwiftUI
+
+struct BeantownButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BeantownButton()
+}
---
+++ Components/List View/MapItemDetails.swift
@@ -0,0 +1,18 @@
+//
+//  MapItemDetails.swift
+//  Components
+//
+//  Created by Avinash on 05/11/2024.
+//
+
+import SwiftUI
+
+struct MapItemDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapItemDetails()
+}
